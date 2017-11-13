7757ba960fcc7feafb48d482511a68b5efcd55e3
Murilo Melo <gamboamurilo@gmail.com>
2017-11-13 18:13:31 -0200

    Menu vanilla js

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Menu vanilla JS
+
+### Método de uso
+
+**HTML**
+
+Coloque os seguintes data-attributes em seu HTML element:
+
+data-menu-active="" 
+
+data-opacity="0"
+
+
+
+Em seguida, você deve adicionar qual será sua lista a ser exibida, e definir ao menos dois botões para que ela seja aberta/fechada:
+
+ul data-collapse="collapse-menu" data-menu-id="0" data-menu-active=""
+
+button data-menu="open" data-menu-id="0"
+
+button data-menu="close" data-menu-id="0"
+
+
+*A estilização em CSS fica a seu critério*
+
+*Cada menu e seus respectivos items devem conter id's únicas, sugerimos que sigam com as id's como sequência em "0, 1, 2, 3, 4"*
---
+++ menu.js
@@ -0,0 +1,41 @@
+function menu(){
+    //Declaração de variáveis
+    var doc = document.documentElement;
+    var openMenus = document.querySelectorAll("button[data-menu=open]");
+    var closeMenus = document.querySelectorAll("button[data-menu=close]");
+    var menus = document.querySelectorAll("ul[data-collapse=collapse-menu]");
+    //Varrer o array de menus coletando suas id's e usando para as funções dos botões
+    for(var i = 0; i < menus.length; i++){
+        //Declaração da variável id para selecionar os menus corretamente
+        openMenus[i].onclick = function(){
+            var id = parseFloat(this.dataset.menuId);
+            doc.setAttribute("data-menu-active", "active");
+            menus[id].setAttribute("data-menu-active", "active");
+            setTimeout(function(){
+                doc.setAttribute("data-opacity", "1"); 
+            },50);
+        };
+        closeMenus[i].onclick = function(){
+            var id = parseFloat(this.dataset.menuId);
+            doc.setAttribute("data-opacity", "0");
+            menus[id].setAttribute("data-menu-active", "");
+            setTimeout(function(){
+                doc.setAttribute("data-menu-active", "");
+            },600);
+        };
+    };
+    //Fechar qualquer/todos os menus ao clicar no document-overlay
+    doc.addEventListener("click", function(event){
+        //document.documentElement apenas, filhos não são selecionados pela condição
+        if(event.target === document.documentElement){
+            var menusAtivos = document.querySelectorAll("ul[data-menu-active=active]");
+            for(var i = 0; i < menusAtivos.length; i++){
+                menusAtivos[i].setAttribute("data-menu-active", "");    
+            };
+            doc.setAttribute("data-opacity", "0");
+            setTimeout(function(){
+                doc.setAttribute("data-menu-active", "");
+            },600);
+        };
+    });
+}menu();
